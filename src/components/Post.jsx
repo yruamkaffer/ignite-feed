7b62e73cb7508262f91ec34ebf8f4bcec1b6d8a1
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-04-08 22:21:33 -0300

    Add files via upload

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,37 +1,60 @@
-import { Comment } from './Comment'
-import { Avatar } from './Avatar'
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import styles from './Post.module.css'
+import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
-export function Post() {
+import styles from './Post.module.css';
+import { useState } from 'react';
+
+export function Post({author, publishedAt, content}) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    });
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, comments.length + 1]);
+    }
+
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/yruamkaffer.png" alt="Foto de perfil do usuário" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Yruam Käffer de Faria</strong>
-                        <span>Projetista</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title='11 de maio às 08:13:00' dateTime="2022-05-11 08:00:00">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-
-                <p>Acabei de subir mais um projeto no meu portifa.
-                É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p>👉{' '}<a href="$">jane.design/doctorcare</a></p>
-
-                <p><a href="#">#novoprojeto</a>{' '}
-                <a href="#">#nlw</a>{' '}
-                <a href="#">#rocketseat</a></p>{' '}
+                {content.map(line => {
+                    if (line.type == 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type == 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={(handleCreateNewComment)} className={styles.commentForm}>
                 <strong> Deixe seu feedback!</strong>
 
                 <textarea placeholder='Deixe um comentário'></textarea>
@@ -42,11 +65,9 @@ export function Post() {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-
-                <Comment />
-
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
 
         </article>
