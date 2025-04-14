3ab2d7eec22796c72e8215570970af414a182b49
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-04-13 22:31:51 -0300

    Add files via upload

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,8 +31,22 @@ export function Post({author, publishedAt, content}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+    }
+
+    function deleteComment(commentToDelete) { 
+        const commnetsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        });
+        setComments(commnetsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return(
         <article className={styles.post}>
@@ -68,16 +82,18 @@ export function Post({author, publishedAt, content}) {
                 placeholder='Deixe um comentário'
                 value={newCommentText}
                 onChange={handleNewCommentChange}
+                onInvalid={handleNewCommentInvalid}
+                required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment} />
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
                 })}
             </div>
 
